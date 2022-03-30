d60eca5ccb90b7da3f111c19fe9c25a0301bd54c
JiHwonChoi <86228531+JiHwonChoi@users.noreply.github.com>
2022-03-30 12:24:26 +0900

    0330

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/Header'
+import Header from './components/Header'
+import Main from './components/Main'
+import Page1 from './components/Page1'
+import NotFound from './components/NotFound';
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div className="App">
+      {/* <Main /> */}
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+          <Route path="/Page1" element={<Page1></Page1>}></Route>
+        </Routes>
+
+      </BrowserRouter>
+      
+    </div>
+  );
+};
+
+export default App;
