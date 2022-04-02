4c4d25b15b4327c11d520f00d04464ca058892bd
JiHwonChoi <86228531+JiHwonChoi@users.noreply.github.com>
2022-04-02 22:17:39 +0900

    위치전송, 마커표시

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,28 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
-const Main = (props) => {
-    //     constructor (props){
-    //         super(props);
-    //         this.imgClick=this.imgClick.bind(this);
-    //     }
+class Main extends Component {
+        constructor (props){
+            super(props);
+            this.state={
+                marker:'marker.png',
+                clicked:{x:0, y:0},
+                    
+                where:{
+                    top: '50%', left: '50%'
+                }
+            }
 
-    //    imgClick(e){
-    //         console.log('click!');
-    //    }
+    
+        }
 
-   
+    
+
+       imgClick(e){
+            console.log(e);
+            console.log('click!');
+            console.log(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+            this.setState(
+                {
+                    clicked:{x:e.nativeEvent.offsetX, y: e.nativeEvent.offsetY},
+                    where:{left:(e.pageX-25) +'px',top: (e.pageY-50) +'px' }
+                }
+            )
+       }
+
+       render(){
+
+
+
         return(
             <div>
 			<h1>main page</h1>
             <h3>Current location</h3>
-            <img src='mapimg/map.png'></img>
+            <img src='mapimg/map.png' onClick={this.imgClick.bind(this)}></img>
+            <img className='marker' src={this.state.marker} alt='hello' style={this.state.where}></img>
+            <h3>This is where clicked on image : {this.state.clicked.x} , {this.state.clicked.y}</h3>
+            <h3>This is where markers at: {this.state.where.top}, {this.state.where.left}</h3>
             <Link to ="/Page1">
                 <h3>Page1</h3>
             </Link>
 		    </div>
 
         );
+
+       }
     
 
 }
