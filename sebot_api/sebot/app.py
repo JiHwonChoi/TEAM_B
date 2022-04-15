1bd78f35c0695f6f0f415587d37b3acf317177e4
jungjh404 <jungjh404@gmail.com>
2022-04-16 02:13:48 +0900

    Idle state, Active State when get user input

--- sebot_api/sebot/app.py
+++ sebot_api/sebot/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # _*_ coding: utf-8 _*_
 
-
 import json
 import cv2
+import threading
 from flask import Flask, request
 
 from ros_utils import SeBot
@@ -14,11 +14,11 @@ app = Flask(__name__)
 sebot = SeBot()
 db = Database()
 
-
 @app.route("/")
 def hello():
     res = db.execute("SELECT * FROM robot_info")
     return json.dumps(res)
+
 
 @app.route("/odom", methods=['GET'])
 def odom():
@@ -32,32 +32,47 @@ def odom():
 
     return res_body
 
+
 @app.route("/get_image", methods=['POST'])
 def get_image():
     return 400
+
 
 @app.route("/call_sebot", methods=['POST'])
 def call_sebot():
     if request.headers["Content-Type"] != "application/json":
         return "INVALID_ACCESS", 406
     
-    data = json.loads(request.get_data())
+    data = json.loads(request.get_data()) # json error detector needed
 
-    if not 'target' in data:
+    if (not 'target' in data) or (not 'start' in data):
         return "INVALID_INPUT", 406
 
-    target = data['target']
+    start_point = data['start']
+    target_point = data['target']
+    end_point = start_point[:]
 
-    if not type(target) is list or len(target) != 2:
+    if not type(start_point) is list or len(start_point) != 2:
         return "INVALID_INPUT", 406
+
+    if not type(target_point) is list or len(target_point) != 2:
+        return "INVALID_INPUT", 406
+
+    if not sebot.idle:
+        return "SEBOT_BUSY", 423
 
     # SEND TARGET TO ROBOT
-    sebot.publish_goal(target)
-
-    # UPDATE ROBOT STATUS TO DB
+    sebot.idle = False
+    sebot.reached = -1
+    sebot.user_path.append(start_point)
+    sebot.user_path.append(target_point)
+    sebot.user_path.append(end_point)
 
     return "SUCCESS", 200
 
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    sebot_thread = threading.Thread(target=sebot.run)
+    flask_thread = threading.Thread(target=app.run, args=("0.0.0.0", 5000))
+    sebot_thread.start()
+    flask_thread.start()
--- sebot_api/sebot/ros_utils.py
+++ sebot_api/sebot/ros_utils.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 # _*_ coding: utf-8 _*_
 
+from re import S
+
+from soupsieve import closest
 import rospy
 from sensor_msgs.msg import CompressedImage
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped
+from move_base_msgs.msg import MoveBaseActionResult
 
 class SeBot:
     def __init__(self):
+        # robot pose
         self.x = None
         self.y = None
         self.z = None
@@ -17,15 +22,106 @@ class SeBot:
         self.qz = None
         self.qw = None
 
+        # robot image
         self.img = None
+        
+        # goal reached topic
+        self.reached = -1
+
+        # robot status => should be handled by flask
+        self.idle = True
+
+        # default path
+        self.path = [[-0.615, 10.210], [-6.511, 10.224], [-8.757, -36.167], [1.814, -26.819]]
+        self.path_couter = 0
+
+        self.user_path = []
 
         rospy.init_node('sebot_api')
         self.odom_subscriber = rospy.Subscriber("/odom", Odometry, self.odom_callback)
         self.image_subscrber = rospy.Subscriber("/camera/rgb/image_raw/compressed", CompressedImage, self.image_callback)
+        self.goal_subscriber = rospy.Subscriber("/move_base/result", MoveBaseActionResult, self.result_callback)
+
         self.goal_publisher = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=1)
+
         # emergency_client
         # log subscriber
-        # goal publisher
+
+    def run(self):
+        while not rospy.is_shutdown():
+            if self.x is None or self.y is None:
+                continue
+            
+            if self.idle:
+                if self.reached == 0:
+                    continue
+
+                else:
+                    rospy.loginfo("Sebot is IDLE")
+                    if self.reached == -1:
+                        self.path_couter = self.get_closest()
+
+                    msg = self.make_goal(self.path[self.path_couter])
+
+                    self.goal_publisher.publish(msg)
+                    self.path_couter = (self.path_couter + 1) % 4
+                    self.reached = 0
+
+            else:
+                if self.reached == 0:
+                    continue
+
+                else:
+                    rospy.loginfo("Sebot is Active")
+                    if len(self.user_path) == 0:
+                        self.idle = True
+                        self.reached = -1
+                        continue
+
+                    msg = self.make_goal(self.user_path[0])
+                    self.goal_publisher.publish(msg)
+                    self.user_path.pop(0)
+                    self.reached = 0
+    
+
+    def make_goal(self, point):
+        msg = PoseStamped()
+        msg.header.frame_id = "map"
+
+        msg.pose.position.x = point[0]
+        msg.pose.position.y = point[1]
+
+        msg.pose.orientation.z = 1
+
+        return msg
+
+
+    def get_closest(self):
+        closest_point = -1
+        min_dist = None
+
+        cur_point = [self.x, self.y]
+
+        for i in range(len(self.path)):
+            point = self.path[i]
+            dist = (point[0] - cur_point[0])**2 + (point[1] - cur_point[1])**2
+
+            if not min_dist:
+                min_dist = dist
+                closest_point = i
+                continue
+
+            elif min_dist > dist:
+                min_dist = dist
+                closest_point = i
+
+        return closest_point
+
+
+    def result_callback(self, msg: MoveBaseActionResult):
+        if "Goal reached" in msg.status.text:
+            self.reached = 1
+
 
     def odom_callback(self, msg: Odometry):
         self.x = msg.pose.pose.position.x
@@ -37,8 +133,10 @@ class SeBot:
         self.qz = msg.pose.pose.orientation.z
         self.qw = msg.pose.pose.orientation.w
 
+
     def image_callback(self, msg: CompressedImage):
         self.img = msg.data
+
 
     def publish_goal(self, goal):
         msg = PoseStamped()
