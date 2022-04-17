225b306726fd8a7f81fb11027bec84c1f444456c
skjunseo0103 <hazzisss5249@gmail.com>
2022-04-17 15:28:39 +0900

    login_register_final

--- login/myproject/app.py
+++ login/myproject/app.py
@@ -10,10 +10,48 @@ def root():
 @app.route('/main')
 def main():
     return render_template('main.html')
+@app.route('/register_form')
+def register_form():
+    return render_template('register/register_form.html')
+
+
+@app.route('/register_proc', methods=['POST']) # 회원가입 화면
+def register():
+    error = None
+    if request.method == 'POST': # POST 형식으로 요청할 것임
+        # 페이지에서 입력한 값을 받아와 변수에 저장
+        userId = request.form['regi_id'] #userid
+        userPwd = request.form['regi_pw'] #userpassword
+        userName = request.form['regi_name'] # userName
+        userCode = request.form['regi_code'] # usercode
+        userNumber = request.form['regi_number'] # usernumber
+        ## 변수를 추가
+        conn = db # DB와 연결
+        cursor = conn.cursor() # connection으로부터 cursor 생성
+        sql = 'INSERT INTO member_info("user_id", "user_pwd", "user_name", "user_code", "user_number") VALUES (%s, %s, %s, %s, %s)' # 실행할 SQL문
+        cursor.execute(sql,(userId, userPwd, userName, userCode, userNumber)) # 메소드로 전달해 명령문을 실행#
+        data = cursor.rowcount # 실행한 결과 데이터를 꺼냄
+        
+        if data != 0:
+            conn.commit() # 변경사항 저장
+            cursor.close()
+            conn.close()
+            return redirect(url_for('main'))  # 로그인 화면으로 이동
+            
+
+        else:
+            conn.rollback() # 데이터베이스에 대한 모든 변경사항을 되돌림
+            cursor.close()
+            conn.close()
+            return "Register Failed"
+         
+    return redirect(url_for('main')) # 용도 확인
 
 @app.route('/login_form')
 def login_form():
     return render_template('login/login_form.html')
+
+
 
 @app.route('/login_proc', methods=['POST'])
 def login_proc():
@@ -25,8 +63,8 @@ def login_proc():
         else:
             conn = db
             cursor = conn.cursor()
-            #sql = 'select idx, userId, userPwd from member where userId = %s and userPwd = %s'
-            sql = 'select * from member'
+            sql = 'select idx, user_id, user_pwd from member_info where user_id = %s and user_pwd = %s'
+            #sql = 'select * from member'
             cursor.execute(sql, (userId, userPwd))
             rows = cursor.fetchall()
             if len(rows) == 0:
@@ -52,11 +90,11 @@ def getUser(edit_idx):
         return redirect('login_form')
     conn = db
     cursor = conn.cursor()
-    #sql = 'select userCode from member where idx = %s'
-    sql = 'select * from member'
+    sql = 'select user_code from member_info where idx = %s'
+    #sql = 'select * from member'
     cursor.execute(sql, (edit_idx,))
     row = cursor.fetchone()
-    edit_code = row[4]
+    edit_code = row[0]
     #cursor.close()
     #conn.close()
     return render_template('users/user_info.html', edit_idx=edit_idx, edit_code=edit_code)
@@ -65,14 +103,14 @@ def getUser(edit_idx):
 def user_info_edit_proc():
     idx = request.form['idx']
     userPwd = request.form.get('userPwd', False)
-    userId = request.form.get('userId',False)
+    userId = request.form.get('userId', False)
     if len('idx') == 0:
         return 'Edit Data Not Found'
     else:
         conn = db
         cursor = conn.cursor()
-        sql = 'update member set userPwd = %s, userId = %s where idx = %s'
-        cursor.execute(sql,(userPwd, userId, idx))
+        sql = 'update member_info set user_pwd = %s where idx = %s'
+        cursor.execute(sql,(userPwd,idx))
         conn.commit()
         cursor.close()
         conn.close()
@@ -82,6 +120,8 @@ def user_info_edit_proc():
 def logout():
     session.clear()
     return redirect(url_for('main'))
+
+
 
 if __name__ == '__main__':
     app.secret_key = '20200601'
