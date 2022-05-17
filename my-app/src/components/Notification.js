657bcf5c080d3a6277a9871412b8ce7c3e939f37
hazzisss5249 <hazzisss5249@gmail.com>
2022-05-17 16:15:22 +0900

    notification_sub

--- my-app/src/components/Notification.js
+++ my-app/src/components/Notification.js
@@ -1,20 +1,58 @@
-import React from 'react'
+import React,{useState, useEffect, useContext} from 'react'
 
-function createDiv() {
-    // 1. <div> element 만들기
-    const newDiv = document.createElement('div');
-    
-    // 2. <div>에 들어갈 text node 만들기
-    const newText = document.createTextNode('안녕하세요');
-    
-    // 3. <div>에 text node 붙이기
-    newDiv.appendChild(newText);
-    
-    // 4. <body>에 1에서 만든 <div> element 붙이기
-    document.body.appendChild(newDiv);
-  } 
+
 
 function Notification() {
+    
+    //로컬에서 불러오기
+        function getLocalStorage(key){ return JSON.parse(localStorage[key]); }
+
+    //div 테그 만들기
+    function createDiv(key) {
+        // 1. <div> element 만들기
+        const newDiv_map = document.createElement('img');
+        newDiv_map.setAttribute("id", "image_map");
+        const newDiv_picture = document.createElement('img');
+        newDiv_picture.setAttribute("id", "image_picture");
+
+        const newDiv_name = document.createElement('div');
+        var key = 1;
+        var obj = JSON.parse(getLocalStorage(key));
+
+        // 2. <div>에 들어갈 text node 만들기
+        //이름
+        const newname = document.createTextNode(obj.name);
+        //지도
+        console.log("지도", obj.map);
+        console.log("사진", obj.picture);
+
+        document.getElementById("image_map").src = obj.map;
+        document.getElementById("image_picture").src = obj.picture;
+
+
+        // 3. <div>에 text node 붙이기
+        newDiv_name.appendChild(newname);
+
+        // 4. <body>에 1에서 만든 <div> element 붙이기
+        document.body.appendChild(newDiv_name);
+        document.body.appendChild(newDiv_picture);
+        document.body.appendChild(newDiv_picture);
+
+      } 
+    //   매번 길이를 확인해서, local storage length 를 통해서 계속 제작해준다.
+
+
+    useEffect( ()=>{ //화면이 열리자 마자 실행이 된다.
+        var step
+        for (step = 1; step <= localStorage.length; step++){
+            //createDIV 가 계속 실행되게 해야 한다.
+            createDiv(step);
+        }
+
+    }, [])
+
+
+
 
     return (
         
--- my-app/src/components/Start_admin.js
+++ my-app/src/components/Start_admin.js
@@ -10,6 +10,7 @@ import Navigation from './Navigation';
 import Register from './Register';
 import axios from 'axios';
 import Login from './Login';
+import { socket } from '../service/socket';
 
 
 
@@ -17,47 +18,82 @@ import Login from './Login';
 
 function StartAdmin (props) {
         
-        const [article, setArticle] = useState(<HomeAdmin />)
-        const [title, setTitle] = useState('this is admin app')
+    const [article, setArticle] = useState(<HomeAdmin />)
+    const [title, setTitle] = useState('this is admin app')
 
-        return(
-            <div>
-                {console.log('render')}
-                <div className='home-background'>
+    //로컬에 저장
+    function setLocalStorage(key, value){ localStorage.setItem(key,JSON.stringify(value)); }
+
+    //socket 으로 받기
+    //1. 사용자 이름이 넘어오기 username
+    // 2. 넘어진 사진 넘어오기 picture
+    // 3. 지도 넘어오기 map 
+    //지도, 사용자 이름, emergency 사진
+    socket.emit("emergency test")
+    socket.on('emergency', (msg) => {
+        // console.log('received')
+        console.log(msg)
+        var arraymap = new Uint8Array( msg.map );
+        var arraypicture = new Uint8Array( msg.picture );
+        var blob_map = new Blob( [ arraymap ], { type: "image/jpeg" } );
+        var blob_picture = new Blob( [ arraypicture ], { type: "image/jpeg" } );
+
+        var urlCreator = window.URL || window.webkitURL;
+        //담아야 되는 3가지 
+        var imageUrl_map = urlCreator.createObjectURL( blob_map );
+        var imageUrl_picture = urlCreator.createObjectURL( blob_picture );
+        var arrayusername = msg.username ;
+
+        const alarm_list = {
+            //이름 넣기
+            name: arrayusername,
+            map: imageUrl_map,
+            picture: imageUrl_picture,
+        }
+
+
+        var jsonData = JSON.stringify(alarm_list);
+        setLocalStorage(localStorage.length, alarm_list)
+    })
+
+    return(
+        <div>
+            {console.log('render')}
+            <div className='home-background'>
+                
+                {title}
+                <br></br><br></br><br></br>
+                {article}
+                <Navigation onChange={function(idx){
+                    console.log('this is onChange function',idx)
+                    if(idx=='plus'){
+                        setArticle(<New />)
+                    }
+                    else if (idx=='search'){
+                        setArticle(<Search />)
+                    }
+                    else if (idx=='home_button'){
+                        setArticle(<HomeAdmin />)
+                    }
+                    else if (idx=='noti'){
+                        setArticle(<Notification />)
+                    }
+                    else if (idx=='profile'){
+                        setArticle(<Profile />)
+                    }
+                    else{
+                        setArticle(<HomeAdmin />)
+                    }
                     
-                    {title}
-                    <br></br><br></br><br></br>
-                    {article}
-                    <Navigation onChange={function(idx){
-                        console.log('this is onChange function',idx)
-                        if(idx=='plus'){
-                            setArticle(<New />)
-                        }
-                        else if (idx=='search'){
-                            setArticle(<Search />)
-                        }
-                        else if (idx=='home_button'){
-                            setArticle(<HomeAdmin />)
-                        }
-                        else if (idx=='noti'){
-                            setArticle(<Notification />)
-                        }
-                        else if (idx=='profile'){
-                            setArticle(<Profile />)
-                        }
-                        else{
-                            setArticle(<HomeAdmin />)
-                        }
-                        
 
 
-                    } }></Navigation>
-                </div>
+                } }></Navigation>
+            </div>
 
-			
-		    </div>
+        
+        </div>
 
-        );
+    );
     
 
 }
