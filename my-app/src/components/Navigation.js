156c12496949abb2688713d782fc641741f8d8ed
JiHwonChoi <luxuryhwon@g.skku.edu>
2022-05-19 16:22:32 +0900

    navigation 간격 수정

--- my-app/src/components/Navigation.js
+++ my-app/src/components/Navigation.js
@@ -8,14 +8,12 @@ const Navigation =(props) => {
         return(
             <div className='Navigation'>
                 <div className='tabbar'>
-                    <div className='plus' onClick={()=> props.onChange('plus')} >
-                        <div className='plus_icon'></div>  
-                    </div>
-                    {/* <div className='search' onClick={()=> props.onChange('search')}>
-                        <div className='search_icon'></div>
-                    </div> */}
                     <div className='home-button' onClick={()=> props.onChange('home_button')}>
                         <div className='home_icon'></div>
+                    </div>
+                    
+                    <div className='plus' onClick={()=> props.onChange('plus')} >
+                        <div className='plus_icon'></div>  
                     </div>
                     <div className='noti' onClick={()=> props.onChange('noti')}>
                         <div className='noti_icon'></div>
