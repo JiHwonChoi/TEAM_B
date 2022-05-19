e64330fc94015edf1c1c6748fb03baa3fff14de1
jungjh404 <jungjh404@gmail.com>
2022-05-19 16:43:56 +0900

    merged app.py

--- sebot_api/sebot/app.py
+++ sebot_api/sebot/app.py
@@ -3,9 +3,10 @@
 
 import cv2
 import time
-from flask import Flask, request, session, render_template, redirect, url_for, jsonify
+from flask import Flask, request, session, jsonify
 from flask_socketio import SocketIO
 from flask_cors import cross_origin, CORS
+from models import db
 import argparse
 import roslibpy
 import json
@@ -14,23 +15,22 @@ from ros_utils import SeBot
 from ros_utils import SeBot
 from db_utils import Database
 
+
 app = Flask(__name__)
-app.host = '0.0.0.0'
-socketio = SocketIO(app, cors_allowed_origins='*')
-
-@app.route('/main')
-def main():
-    return render_template('main.html')
+cors = CORS(app, resources={r"/*": {"origins": "*"}}, automatic_options=True)
+socketio = SocketIO(app, cors_allowed_origins="*")
+session = {}
 
 
-@app.route('/register_form')
-def register_form():
-    return render_template('register/register_form.html')
+@app.route('/')
+def index():
+    return "<h1> HI </h1>"
 
 
-@app.route('/register_proc', methods=['POST']) # 회원가입 화면
+@app.route('/register', methods=['POST',"GET"]) # 회원가입 화면
 def register():
     if request.method == 'POST': # POST 형식으로 요청할 것임
+    
         # 페이지에서 입력한 값을 받아와 변수에 저장
         userId = request.form['regi_id'] #userid
         userPwd = request.form['regi_pw'] #userpassword
@@ -38,93 +38,83 @@ def register():
         userName = request.form['regi_name'] # userName
         userCode = request.form['regi_code'] # usercode
         userNumber = request.form['regi_number'] # usernumber
-        userType = request.form.get('regi_admin', False) ## userType
-
-        ## 변수를 추가
+  
         if userPwd_check != userPwd:
-            return '비밀번호가 일치하지 않습니다.', 400
-        
-        sql = 'INSERT INTO member_info("user_id", "user_pwd", "user_name", "user_code", "user_number", "user_type") VALUES (%s, %s, %s, %s, %s, %s)' # 실행할 SQL문
+            return jsonify({'ERROR' : 'NOT SAME Password and Check_Password.'}),411
+          
+        conn = db # DB와 연결
+        cursor = conn.cursor() # connection으로부터 cursor 생성
+        sql = 'INSERT INTO member_info("user_id", "user_pwd", "user_name", "user_code", "user_number") VALUES (%s, %s, %s, %s, %s)' # 실행할 SQL문
         
         try:
-            db.execute(sql, (userId, userPwd, userName, userCode, userNumber, userType,))
-            
-        except psycopg2.IntegrityError:
-            db.db.rollback()
-            return f"Register Failed: IntegrityError", 400
+            cursor.execute(sql,(userId, userPwd, userName, userCode, userNumber)) # 메소드로 전달해 명령문을 실행#
+            #conn.commit() # 변경사항 저장
+            return jsonify({'SUCCESS': 'register'}),200  # 로그인 화면으로 이동
         
-    return redirect(url_for('main')) # 용도 확인
+        except:
+            conn.rollback() # 데이터베이스에 대한 모든 변경사항을 되돌림
+            return jsonify({'ERROR' : 'Register Failed'}),415
+         
+    return jsonify({'ERROR' : 'Posting Error'}),416 # 용도 확인
 
 
-@app.route('/login_form')
-def login_form():
-    return render_template('login/login_form.html')
 
-
-@app.route('/login_proc', methods=['POST'])
-def login_proc():
-    userId = request.form['id']
-    userPwd = request.form['pwd']
-    if len(userId) == 0 or len(userPwd) == 0:
-        return 'userId, userPwd not found!!'
-        
-    else:
-        sql = 'SELECT idx, user_id, user_pwd, user_code, user_name FROM member_info WHERE (user_id = %s or user_code = %s) and user_pwd = %s'
-        rows = db.execute(sql, (userId, userId, userPwd,))
-
-        if len(rows) == 0:
-            return redirect(url_for('login_form'))
-
+@app.route('/login', methods=['POST',"GET"])
+def login():
+    if request.method == 'POST':
+        userId = request.form['id']
+        userPwd = request.form['pw']
+        if len(userId) == 0 or len(userPwd) == 0:
+            return jsonify({'ERROR' : 'Please enter your ID and Password'}),400
         else:
-            for rs in rows:
-                if (userId == rs[1] and userPwd == rs[2])or(userId == rs[3] and userPwd == rs[2]): #회원 코드로도 로그인 가능
-                    session['logFlag'] = True
-                    session['idx'] = rs[0]
-                    session['userId'] = rs[4]
-                    
-                    return redirect(url_for('main'))
-
-                else:
-                    return redirect(url_for('login_form')) #메소드를 호출
-
-    
-    return '잘못된 접근입니다.', 400
-
-
-@app.route('/user_info_edit/<int:edit_idx>', methods=['GET'])
-def getUser(edit_idx):
-    if session.get('logFlag') != True:
-        return redirect('login_form')
-
-    sql = 'SELECT user_code FROM member_info WHERE idx = %s'
-    row = db.execute(sql, (edit_idx,))
-    edit_code = row[0][0]
-    return render_template('users/user_info.html', edit_idx=edit_idx, edit_code=edit_code)
-
-
-@app.route('/user_info_edit_proc', methods=['POST'])
-def user_info_edit_proc():
-    idx = request.form['idx']
-    userPwd = request.form.get('userPwd', False)
-    
-    if len('idx') == 0:
-        return 'Edit Data Not Found', 400
-
+            conn = db
+            cursor = conn.cursor()
+            sql = 'select idx, user_id, user_pwd, user_code, user_name, user_type from member_info where (user_id = %s or user_code = %s) and user_pwd = %s'
+            #sql = 'select * from member'
+            cursor.execute(sql, (userId, userId, userPwd))
+            rows = cursor.fetchall()
+            if len(rows) == 0:
+                return jsonify({'ERROR' : 'NOT Exist ID or Password'}),401
+            else:
+                for rs in rows:
+                    if (userId == rs[1] and userPwd == rs[2])or(userId == rs[3] and userPwd == rs[2]): #회원 코드로도 로그인 가능
+                        session['logFlag'] = True
+                        session['idx'] = rs[0]
+                        session['userId'] = rs[4]
+                        session['userType'] = rs[5]
+                        print(session) ## 출력이 되므로 session에 저장이 되어 있음
+                        return jsonify({'SUCCESS': 'login', "data" : session['userType'], "ID": session['userId']}),200
+                    else:
+                        return jsonify({'ERROR' : 'Login Failed'}),400 #메소드를 호출
     else:
-        sql = 'UPDATE member_info SET user_pwd = %s WHERE idx = %s'
-        db.execute(sql, (userPwd, idx))
-        return redirect(url_for('main'))
+        return jsonify({'ERROR' : 'Posting Error'}),402
 
 
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('main'))
 
+@app.route('/info', methods=['POST',"GET"])
+def info():
+
+    if request.method == 'POST':
+        login_status = request.form["login_status"]
+        
+        if login_status != "True" :
+            return jsonify({'ERROR' : 'Not login'}),402
+        else:
+            userId = session["userId"]
+            userType = session["userType"]
+            print(userId)
+            print(userType)
+            try:
+                return jsonify({'SUCCESS': 200, 'Data' : userId, "Type": userType}), 200   
+            except:
+                return jsonify({'ERROR' : 'Load Failed'}), 405
+    else:
+        return jsonify({'ERROR' : 'Posting Error'}),401
+
+## 로그아웃 할 경우에, 다시 로그인 화면으로 돌아가게 하고, session을 clear 한다.
 
 # Send Destination
 @app.route("/call_sebot", methods=['POST'])
-@cross_origin()
 def call_sebot():
     print('call sebot')
     if request.headers["Content-Type"] != "application/json":
@@ -252,11 +242,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--robot-ip", type=str, help="robot's ip")
     args = parser.parse_args()
-
     db = Database()
     sebot = SeBot(db, args.robot_ip, socketio)
-
-    app.secret_key = '20200601'
+    app.secret_key = 'super secret key'
     # app.debug = True
-    # app.run(host="0.0.0.0", port=5000)
-    socketio.run(app)
+    # app.run(port=5000, debug = True)
+    socketio.run(app, debug=True)
