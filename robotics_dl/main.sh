0a7d897666cbfd47c806f41d1fc15b92c6622cdf
danmuzi <sounghyn105@naver.com>
2022-05-19 16:50:41 +0900

    robotics_dl final

--- robotics_dl/main.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /home/danmuzi/catkin_ws/devel/setup.bash
-tmux new-session -d -s human_follower 'python human_follower.py'
-tmux split-window -v -p 20
-tmux new-session -d -s pose_estimator 'python pose_estimator.py --net_resolution 320x-1'
