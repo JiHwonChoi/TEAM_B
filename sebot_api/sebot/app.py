03a5ac57d0a48fb05af491a893dd397164e2118d
jungjh404 <jungjh404@gmail.com>
2022-05-24 19:57:50 +0900

    credential

--- sebot_api/sebot/app.py
+++ sebot_api/sebot/app.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
-cors = CORS(app, resources={r"/*": {"origins": "*"}}, automatic_options=True)
+cors = CORS(app, resources={r"/*": {"origins": "*"}}, automatic_options=True, supports_credentials=True)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
