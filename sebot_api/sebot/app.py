43906e3071e4b77f3e1dd625d1116be034eb8f6b
jungjh404 <jungjh404@gmail.com>
2022-05-24 20:42:01 +0900

    4

--- sebot_api/sebot/app.py
+++ sebot_api/sebot/app.py
@@ -19,8 +19,8 @@ from ros_utils import SeBot
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 Session(app)
-# cors = CORS(app, resources={r"/*": {"origins": "*"}}, automatic_options=True, supports_credentials=True)
-socketio = SocketIO(app, cors_allowed_origins=['http://52.79.237.147:3000/', 'http://localhost:3000'])
+cors = CORS(app, resources={r"/*": {"origins": ['http://52.79.237.147:3000', 'http://localhost:3000']}}, automatic_options=True, supports_credentials=True)
+socketio = SocketIO(app, cors_allowed_origins=['http://52.79.237.147:3000', 'http://localhost:3000'])
 # socketio = SocketIO(app)
 # session = {}
 
