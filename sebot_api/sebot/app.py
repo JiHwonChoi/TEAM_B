f8db99e95c2b2b0974b090c6cea928af7c18ee28
jungjh404 <jungjh404@gmail.com>
2022-05-24 19:51:27 +0900

    filesystem

--- sebot_api/sebot/app.py
+++ sebot_api/sebot/app.py
@@ -20,6 +20,7 @@ from db_utils import Database
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 cors = CORS(app, resources={r"/*": {"origins": "*"}}, automatic_options=True)
 socketio = SocketIO(app, cors_allowed_origins="*")
